96244d5d1cf241ebcbb9a7c91614f7040873a459
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2022-03-10 13:31:12 -0300

    7076 Agrego separadores y modifico el estilo de los metadatos de autor de un documento en la vista de un item

--- src/themes/cicba/app/item-page/simple/field-components/metadata-representation-list/cic-metadata-representation-list.component.ts
+++ src/themes/cicba/app/item-page/simple/field-components/metadata-representation-list/cic-metadata-representation-list.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MetadataRepresentationListComponent } from 'src/app/item-page/simple/metadata-representation-list/metadata-representation-list.component';
 
 @Component({
   selector: 'ds-cic-metadata-representation-list',
+  styleUrls: ['./cic-metadata-representation-list.component.scss'],
   templateUrl: './cic-metadata-representation-list.component.html'
 })
 /**
@@ -12,5 +13,8 @@ import { MetadataRepresentationListComponent } from 'src/app/item-page/simple/me
  * It expects a label to put on top of the list
  */
 export class CicMetadataRepresentationListComponent extends MetadataRepresentationListComponent {
-
+  /**
+   * The separator used to split the metadata values
+   */
+  @Input() separator: string;
 }
