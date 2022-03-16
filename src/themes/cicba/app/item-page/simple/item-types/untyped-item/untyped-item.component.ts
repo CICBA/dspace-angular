21760cd57d37fb3bf3448e1c604911f6561a9a1d
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2022-03-16 12:08:18 -0300

    7296 En la vista de un item agrego parseo al metadato dcterms.identifier.other para poder obtener el tipo de identificador y mostrarlo correctamente en los badges

--- src/themes/cicba/app/item-page/simple/item-types/untyped-item/untyped-item.component.ts
+++ src/themes/cicba/app/item-page/simple/item-types/untyped-item/untyped-item.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Item } from '../../../../../../../app/core/shared/item.model';
 import { ViewMode } from '../../../../../../../app/core/shared/view-mode.model';
 import { listableObjectComponent } from '../../../../../../../app/shared/object-collection/shared/listable-object/listable-object.decorator';
 import { VersionedItemComponent } from '../../../../../../../app/item-page/simple/item-types/versioned-item/versioned-item.component';
 import { Context } from 'src/app/core/shared/context.model';
+import { MetadataValue } from 'src/app/core/shared/metadata.models';
 
 /**
  * Component that represents a publication Item page
@@ -16,6 +17,35 @@ import { Context } from 'src/app/core/shared/context.model';
   templateUrl: './untyped-item.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UntypedItemComponent extends VersionedItemComponent {
+export class UntypedItemComponent extends VersionedItemComponent implements OnInit {
 
+  identifierOtherMetadataName: string = 'dcterms.identifier.other';
+  identifierOtherValues: { mdValue: MetadataValue, label: string }[];
+
+  ngOnInit(): void {
+    this.setIdentifierOtherValues();
+  }
+
+  setIdentifierOtherValues(): void {
+    this.identifierOtherValues = [];
+    this.object.allMetadata([this.identifierOtherMetadataName]).forEach(
+      (mdValue, index) => {
+        var charIndex = -1;
+        var label = "";
+        if (!mdValue.value.startsWith("http")) {
+          var splitChar = mdValue.value.includes(" ") ? " " : ":";
+          charIndex = mdValue.value.indexOf(splitChar);
+          label = mdValue.value.substring(0, charIndex).toUpperCase();
+        } else {
+          label = "URL";
+        }
+        var value = mdValue.value.substring(charIndex + 1).trim();
+        this.identifierOtherValues[index] = {
+          mdValue: new MetadataValue(),
+          label: label
+        }
+        this.identifierOtherValues[index].mdValue.value = value;
+      }
+    );
+  }
 }
