220598d47565e0d5e3c25d592e2a980559e3f20b
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2022-08-18 12:11:24 -0300

    8110 Ahora como nombre de los bitstreams de un item en la vista de un item aparece su descripcion, y en caso de no tenerla se muestra su nombre

--- src/themes/cicba/app/item-page/simple/field-components/file-section/file-section.component.ts
+++ src/themes/cicba/app/item-page/simple/field-components/file-section/file-section.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { slideSidebarPadding } from '../../../../../../../app/shared/animations/slide';
 import { FileSectionComponent as BaseComponent } from '../../../../../../../app/item-page/simple/field-components/file-section/file-section.component';
+import { Bitstream } from 'src/app/core/shared/bitstream.model';
 
 @Component({
     selector: 'ds-item-page-file-section',
@@ -10,4 +11,10 @@ import { FileSectionComponent as BaseComponent } from '../../../../../../../app/
 })
 export class FileSectionComponent extends BaseComponent {
 
+    descriptionMetadataName: string = 'dc.description';
+
+    getFileName(file: Bitstream): string {
+        const fileDescription = file.firstMetadataValue(this.descriptionMetadataName);
+        return fileDescription? fileDescription : file.name;
+    }
 }
