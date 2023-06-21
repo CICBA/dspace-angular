286668c0d5b26c7dd627cfb56f6c75ec5976a790
Pablo de Albuquerque <pablo.cda@gmail.com>
2023-06-21 11:49:02 -0300

    9373 fix selector para subcol y subcom en vista de comunidades

--- src/themes/cicba/app/community-page/sub-community-list/community-page-sub-community-list.component.ts
+++ src/themes/cicba/app/community-page/sub-community-list/community-page-sub-community-list.component.ts
@@ -3,14 +3,13 @@ import { CommunityPageSubCommunityListComponent as BaseComponent }
   from '../../../../../app/community-page/sub-community-list/community-page-sub-community-list.component';
 
 @Component({
-  selector: 'ds-cic-community-page-sub-community-list',
+  selector: 'ds-community-page-sub-community-list',
   // styleUrls: ['./community-page-sub-community-list.component.scss'],
   styleUrls: ['../../../../../app/community-page/sub-community-list/community-page-sub-community-list.component.scss'],
   // templateUrl: './community-page-sub-community-list.component.html',
   templateUrl: '../../../../../app/community-page/sub-community-list/community-page-sub-community-list.component.html'
 })
 export class CommunityPageSubCommunityListComponent extends BaseComponent {
-
   ngOnInit(): void {
     super.ngOnInit();
     this.config.pageSize = 100;
