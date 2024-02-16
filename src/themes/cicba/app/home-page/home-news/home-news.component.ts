06a4d77eb581f53cb48d6025a573c3b00f020584
Felipe Mosqueira <mosqueirafelipe22@gmail.com>
2024-02-16 09:49:22 -0300

    Método que le pega a la API y trae la cantidad total de ítems

--- src/themes/cicba/app/home-page/home-news/home-news.component.ts
+++ src/themes/cicba/app/home-page/home-news/home-news.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Observable, map, startWith, timeout } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { HomeNewsComponent as BaseComponent } from '../../../../../app/home-page/home-news/home-news.component';
 
 @Component({
@@ -10,5 +13,30 @@ import { HomeNewsComponent as BaseComponent } from '../../../../../app/home-page
 /**
  * Component to render the news section on the home page
  */
-export class HomeNewsComponent extends BaseComponent {}
+
+export class HomeNewsComponent extends BaseComponent {
+
+  placeholder$: Observable<string>;
+
+  constructor(private http: HttpClient, private cd: ChangeDetectorRef) { 
+    super();
+    this.placeholder$ = this.getTotalItemCount();
+  }
+
+  private getTotalItemCount(): Observable<string> {
+    const api = `${environment.rest.baseUrl}/api/items/total-count`;
+    return this.http.get(api).pipe(
+      timeout(5000),
+      map(total => this.createPlaceholder(total as number)),
+      startWith("Buscando recursos...")
+    );
+  }
+
+  private createPlaceholder(total: number): string {
+    // const placeholder = `Busque entre los ${total} recursos disponibles en el repositorio`;
+    const placeholder = `Buscar en el repositorio (${total} ítems disponibles)`;
+    this.cd.detectChanges();
+    return placeholder;
+  }
+}
 
